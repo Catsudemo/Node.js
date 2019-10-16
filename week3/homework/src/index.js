6b46d1cc394f769d97aa3cb860b09011aa2816b3
Catsudemo <cattmott@gmail.com>
2019-10-16 21:21:25 +0200

    Swapped put for post
    Swapped put for post for updating and marking as done.

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -31,7 +31,7 @@ app.post(`/${TODO_SLUG}`, createTodo.bind(null, todo));
 app.get(`/${TODO_SLUG}`, readTodos.bind(null, todo));
 app.get(`/${TODO_SLUG}/:id`, readTodo.bind(null, todo));
 app.put(`/${TODO_SLUG}/:id`, updateTodo.bind(null, todo));
-app.put(`/${TODO_SLUG}/:id/done`, markAsDone.bind(null, todo));
+app.post(`/${TODO_SLUG}/:id/done`, markAsDone.bind(null, todo));
 app.delete(`/${TODO_SLUG}/:id/done`, markAsNotDone.bind(null, todo));
 app.delete(`/${TODO_SLUG}/:id`, deleteTodo.bind(null, todo));
 app.delete(`/${TODO_SLUG}/`, clearTodos.bind(null, todo));
